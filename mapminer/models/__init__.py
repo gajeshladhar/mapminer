e70acc8670540bcf133700a2fd242df23d318adf
Gajesh Ladhar <gajeshladhar@gmail.com>
2025-08-26 07:45:57 +0000

    DiNOv3 model architecture & weights added

--- mapminer/models/__init__.py
+++ mapminer/models/__init__.py
@@ -1,6 +1,13 @@
 import torch
 from .nafnet import NAFNet
 from .convlstm import ConvLSTM
+from .dinov3 import DiNOV3
+
+
+# Aliases for convenience
+DINOv3 = DiNOV3
+DINOV3 = DiNOV3
+DiNOv3 = DiNOV3
 
 if __name__=="__main__":
     model = NAFNet(in_channels=12)
